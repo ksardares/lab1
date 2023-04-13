c948604489175b7c96b373b80fc78a4722274f98
ksardares <kostyantyno54@gmail.com>
2023-04-13 09:27:26 +0300

    Initial commit

---
+++ sections/header.php
@@ -0,0 +1,7 @@
+<h1>This is the page header!</h1>
+
+<ul>
+    <li><a href="/lab1/?task=1">Task 01</a></li>
+    <li><a href="/lab1/?task=2">Task 02</a></li>
+    <li><a href="/lab1/?task=3">Task 03</a></li>
+</ul>
